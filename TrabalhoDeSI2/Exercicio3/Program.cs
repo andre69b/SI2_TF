438a9c79ed426520ee1e8e14926a86205ba26474
andre69b <andrebasilio123@gmail.com>
2014-01-13 17:26:28 +0000

    Alinea 2 feita!!

--- TrabalhoDeSI2/Exercicio3/Program.cs
+++ TrabalhoDeSI2/Exercicio3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,28 @@ namespace Exercicio3
 {
     class Program
     {
+
+        private static Boolean ponto2(int points,int clienteId)
+        {
+            using (var ctx = new SI2_1314i_TPEntities())
+            {
+                var q = (from a in ctx.Cliente 
+                         where a.nr_cliente == clienteId 
+                         select a
+                        );
+                foreach (var cliente in q)
+                {
+                    cliente.pontos += points;
+                }
+                ctx.SaveChanges();
+            }
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            
+            ponto2(2,0);
         }
     }
 }
