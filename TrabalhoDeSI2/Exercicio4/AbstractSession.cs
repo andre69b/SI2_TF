6aeb62077f4742d14dbb0c1e97a9a5d4ec652d75
andre69b <andrebasilio123@gmail.com>
2014-01-13 22:21:50 +0000

    Estrutura das transações e connecções para o exercicio 4

---
+++ TrabalhoDeSI2/Exercicio4/AbstractSession.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public abstract class AbstractSession : ISession
+    {
+        private SqlTransaction currentTrans = null;
+        private SqlConnection currentConn = null;
+        private string cs;
+        private bool TransactionVotes;
+
+        public AbstractSession()
+        {
+            cs = ConfigurationManager.ConnectionStrings["SI2_1314i_TPEntities"].ConnectionString;
+        }
+
+        public bool BeginTran()
+        {
+            bool st = false;
+            if (currentConn == null)
+            {
+                throw new Exception("Connection is closed");
+            }
+            if (currentTrans == null)
+            {
+                currentTrans = currentConn.BeginTransaction();
+                TransactionVotes = true;
+                st = true;
+            }
+            return st;
+        }
+
+        public void EndTransaction(bool MyVote, bool isMyTransaction)
+        {
+            TransactionVotes &= MyVote;
+            if (isMyTransaction)
+            {
+                if (TransactionVotes)
+                    currentTrans.Commit();
+                else
+                    currentTrans.Rollback();
+                currentTrans = null;
+            }
+
+        }
+
+        public bool OpenConnection()
+        {
+            bool sc = false;
+            if (currentConn == null)
+            {
+                currentConn = new SqlConnection(cs);
+                currentConn.Open();
+                sc = true;
+            }
+            return sc;
+        }
+
+        public void CloseConnection(bool isMyConnection)
+        {
+            if (isMyConnection && currentConn != null)
+            {
+                currentConn.Close();
+                currentConn = null;
+            }
+        }
+
+        public SqlCommand CreateCommand()
+        {
+            SqlCommand cmd = currentConn.CreateCommand();
+
+            cmd.Transaction = currentTrans;
+
+            return cmd;
+        }
+
+        public void Dispose()
+        {
+            if (currentConn != null)
+                currentConn.Dispose();
+        }
+    }
+}
---
+++ TrabalhoDeSI2/Exercicio4/ISession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public interface ISession : IDisposable
+    {
+
+        bool BeginTran();
+        bool OpenConnection();
+        void EndTransaction(bool MyVote, bool isMyTransaction);
+        void CloseConnection(bool isMyTransaction);
+
+        SqlCommand CreateCommand();
+    }
+}
